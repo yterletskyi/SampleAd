0e9019b9222e651e4b2fdc49caaee69d8844aadb
Yura <yterletskyi@gmail.com>
2017-08-05 12:45:35 +0300

    Remove hardcoded vast xml

--- app/src/main/java/com/publisher/sample/videoplayerapp/MyActivity.java
+++ app/src/main/java/com/publisher/sample/videoplayerapp/MyActivity.java
@@ -76,58 +76,7 @@ public class MyActivity extends AppCompatActivity {
 
     private void formVastXml(PreloadResponse result) {
         VastCreator vastCreator = new VastCreator();
-//        String xml = vastCreator.build(result);
-        String xml =
-                "<VAST version=\"2.0\">\n" +
-                        "\t<Ad id=\"5979ec4b80aedd125355720a\">\n" +
-                        "\t\t<InLine>\n" +
-                        "\t\t\t<AdSystem>DSAF</AdSystem>\n" +
-                        "\t\t\t<AdTitle>REQUIRED FIELD</AdTitle>\n" +
-                        "\t\t\t<Impression>\n" +
-                        "\t\t\t\t<![CDATA[asd]]>\n" +
-                        "\t\t\t</Impression>\n" +
-                        "\t\t\t<Creatives>\n" +
-                        "\t\t\t\t<Creative>\n" +
-                        "\t\t\t\t\t<Linear>\n" +
-                        "\t\t\t\t\t\t<Duration>00:00:10</Duration>\n" +
-                        "\t\t\t\t\t\t<TrackingEvents>\n" +
-                        "\t\t\t\t\t\t\t<Tracking event=\"start\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://ingest.vungle.com/tpat?event_id=5979ec4b80aedd125355720a&device_id=d3ca6224c70c7219&event_type=PERCENTAGE&play_percentage=0]]>\n" +
-                        "\t\t\t\t\t\t\t</Tracking>\n" +
-                        "\t\t\t\t\t\t\t<Tracking event=\"firstQuartile\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://ingest.vungle.com/tpat?event_id=5979ec4b80aedd125355720a&device_id=d3ca6224c70c7219&event_type=PERCENTAGE&play_percentage=0.25]]>\n" +
-                        "\t\t\t\t\t\t\t</Tracking>\n" +
-                        "\t\t\t\t\t\t\t<Tracking event=\"midpoint\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://ingest.vungle.com/tpat?event_id=5979ec4b80aedd125355720a&device_id=d3ca6224c70c7219&event_type=PERCENTAGE&play_percentage=0.5]]>\n" +
-                        "\t\t\t\t\t\t\t</Tracking>\n" +
-                        "\t\t\t\t\t\t\t<Tracking event=\"thirdQuartile\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://ingest.vungle.com/tpat?event_id=5979ec4b80aedd125355720a&device_id=d3ca6224c70c7219&event_type=PERCENTAGE&play_percentage=0.75]]>\n" +
-                        "\t\t\t\t\t\t\t</Tracking>\n" +
-                        "\t\t\t\t\t\t\t<Tracking event=\"complete\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://ingest.vungle.com/tpat?event_id=5979ec4b80aedd125355720a&device_id=d3ca6224c70c7219&event_type=PERCENTAGE&play_percentage=1]]>\n" +
-                        "\t\t\t\t\t\t\t</Tracking>\n" +
-                        "\t\t\t\t\t\t\t<Tracking event=\"mute\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://ingest.vungle.com/tpat?event_id=5979ec4b80aedd125355720a&device_id=d3ca6224c70c7219&event_type=MUTE]]>\n" +
-                        "\t\t\t\t\t\t\t</Tracking>\n" +
-                        "\t\t\t\t\t\t\t<Tracking event=\"unmute\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://ingest.vungle.com/tpat?event_id=5979ec4b80aedd125355720a&device_id=d3ca6224c70c7219&event_type=UNMUTE]]>\n" +
-                        "\t\t\t\t\t\t\t</Tracking>\n" +
-                        "\t\t\t\t\t\t\t<Tracking event=\"closeLinear\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://ingest.vungle.com/tpat?event_id=5979ec4b80aedd125355720a&device_id=d3ca6224c70c7219&event_type=VIDEO_CLOSE]]>\n" +
-                        "\t\t\t\t\t\t\t</Tracking>\n" +
-                        "\t\t\t\t\t\t</TrackingEvents>\n" +
-                        "\t\t\t\t\t\t<MediaFiles>\n" +
-                        "\t\t\t\t\t\t\t<MediaFile delivery=\"progressive\" width=\"960\" height=\"540\" type=\"video/mp4\">\n" +
-                        "\t\t\t\t\t\t\t\t<![CDATA[https://cdn-lb.vungle.com/zen/GP0004_MCOC_mashup-DP-Hulk-SG-IF-SL-MagC_30s_1920x1080_UI_duo-960x540-Q2.mp4]]>\n" +
-                        "\t\t\t\t\t\t\t</MediaFile>\n" +
-                        "\t\t\t\t\t\t</MediaFiles>\n" +
-                        "\t\t\t\t\t</Linear>\n" +
-                        "\t\t\t\t</Creative>\n" +
-                        "\t\t\t</Creatives>\n" +
-                        "\t\t</InLine>\n" +
-                        "\t</Ad>\n" +
-                        "</VAST>";
-
+        String xml = vastCreator.build(result);
         setVastXml(xml);
         enablePlayButton();
     }
