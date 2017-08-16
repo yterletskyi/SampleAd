b44f151801f8af2deaa88e24cb336f31c41606d6
Yura <yterletskyi@gmail.com>
2017-08-16 19:04:03 +0300

    Add sending report_ad request

---
+++ vunglesdk/src/main/java/yterletskyi/com/vunglesdk/sdk/model/reportad/response/ReportAdResponse.java
@@ -0,0 +1,17 @@
+package yterletskyi.com.vunglesdk.sdk.model.reportad.response;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by yterletskyi on 16.08.17.
+ */
+
+public class ReportAdResponse {
+
+    @SerializedName("msg")
+    public String message;
+
+    @SerializedName("code")
+    public int code;
+
+}
